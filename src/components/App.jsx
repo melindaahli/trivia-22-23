afc36146ea91ce230b01b1e9a761e9d51247143b
masalaza-cn <83977863+masalaza-cn@users.noreply.github.com>
2022-11-26 00:30:24 -0500

    Delete App.jsx

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component, useState } from "react";
-import "../css/App.css";
-import data from "../sample_data.json";
-
-function App() {
-  return <div className="app">Trivia!</div>;
-}
-
-export default App;
